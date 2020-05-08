8f30572ecd8bc537a6e18e30753d3746ce58a4f3
gjihan <gjihan97@gmail.com>
2020-05-08 17:02:10 +0800

    config:init base configs

---
+++ base/src/main/java/com/jihan/wanandroid/base/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.jihan.wanandroid.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @Author Jihan
+ * @Date 2020/5/8 16:59
+ * @Description
+ */
+
+abstract class BaseActivity():AppCompatActivity()
